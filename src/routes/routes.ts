30839692467be27da0d9568821854c8ee8871fb3
unknown <adiguzelburak@icloud.com>
2024-05-13 21:54:11 +0300

    english and turkish dummy data is added.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
+import tr from "../data/tr";
+import en from "../data/en";
 
 const router = express.Router();
 
 router.get("/en", (req: Request, res: Response) => {
-  return res.send("english");
+  return res.send(en);
 });
 
 router.get("/tr", (req: Request, res: Response) => {
-  return res.send("türkçe");
+  return res.send(tr);
 });
 
 export { router as routers };
