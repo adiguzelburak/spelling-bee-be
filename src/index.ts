e4f12e30503d7706c0c723caae041086178ca8d3
unknown <adiguzelburak@icloud.com>
2024-05-15 14:05:12 +0300

    heroku settings

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
 import { routers } from "./routes/routes";
+import { config } from "../config";
 
 const app = express();
 app.use(json());
 app.use(routers);
 
-app.listen(3003, () => {
-  console.log(`SERVER WORKS AT: ${3003}`);
+app.listen(config.server.port, () => {
+  console.log(`SERVER WORKS AT: ${config.server.port}`);
 });
